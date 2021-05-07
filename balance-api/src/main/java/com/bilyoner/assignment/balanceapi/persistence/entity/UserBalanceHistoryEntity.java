4095eddaa19eeaefe6744b7283c7f9d0a5778258
caner.kaya@n11.com <caner.kaya@n11.com>
2021-05-07 12:25:30 +0300

    fix

--- balance-api/src/main/java/com/bilyoner/assignment/balanceapi/persistence/entity/UserBalanceHistoryEntity.java
+++ balance-api/src/main/java/com/bilyoner/assignment/balanceapi/persistence/entity/UserBalanceHistoryEntity.java
@@ -1,21 +1,17 @@
 package com.bilyoner.assignment.balanceapi.persistence.entity;
 
+import com.bilyoner.assignment.balanceapi.model.enums.TransactionType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.util.List;
 
 @Builder
 @Setter
@@ -39,5 +35,5 @@ public class UserBalanceHistoryEntity {
     private String transactionId;
 
     @Column(nullable = false)
-    private String transactionType;
+    private TransactionType transactionType;
 }
