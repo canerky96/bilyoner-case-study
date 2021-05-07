288b6c4269ff5e43c1721c2ff3f60d0784d54d77
Caner Kaya <canerky96@gmail.com>
2021-05-07 20:52:19 +0300

    test added for contextLoadTest

--- balance-api/src/test/java/com/bilyoner/assignment/balanceapi/BalanceApiApplicationTest.java
+++ balance-api/src/test/java/com/bilyoner/assignment/balanceapi/BalanceApiApplicationTest.java
@@ -1,13 +1,23 @@
 package com.bilyoner.assignment.balanceapi;
 
+import com.bilyoner.assignment.balanceapi.controller.BalanceController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class BalanceApiApplicationTest {
+
+    @Autowired
+    private BalanceController balanceController;
 
     @Test
     public void contextLoadTest() {
+        assertThat(balanceController).isNotNull();
     }
 }
